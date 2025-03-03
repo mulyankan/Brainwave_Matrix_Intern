a3be0ac52eb571d6a8bd067bf3c3c54ba9df1822
Mulyankan Sharma <69049187+mulyankan@users.noreply.github.com>
2025-03-03 21:32:55 +0545

    Update program.py

--- program.py
+++ program.py
@@ -1,17 +1,1 @@
- #phising URL checker using python
 
- #listing the urls
-phising_urls = ["revil,ru", "coti.ru", "doubledragon.cn", "ryuk.ru", "spam.com"]
-
-#user being asked to enter the url
-url = input("Enter the url: ")
-
-domain = url.split("//")[-1].split("//")[0]
-
-#checking the condition
-
-if domain in phising_urls:
-    print("Alert!, the URL is known phising URL.")
-else:
-    print("The URL doesn't apear to be a phising URL.")
-    
